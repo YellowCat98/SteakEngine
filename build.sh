9d4f87607e84f08023cebeb5c9e54648e65d2510
YellowCat98 <150300904+YellowCat98@users.noreply.github.com>
2025-01-01 21:30:01 +0100

    what the actual fuck is happening

--- build.sh
+++ build.sh
@@ -11,9 +11,4 @@ git clone https://github.com/lua/lua.git lib/lua
 cmake -S . -B build -G Xcode
 if [ $? -eq 0 ]; then
     cmake --build build --config Release
-    if [ $? -eq 1 ]; then
-        echo "Build failed.\n"
-    else
-        echo "Build succeeded!\n"
-    fi
 fi

75bb243a27130ebb6c6a65606f9dc7148804d242
YellowCat98 <150300904+YellowCat98@users.noreply.github.com>
2025-01-01 20:43:46 +0100

    add a build.sh and use that to build

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! command -v cmake &> /dev/null
+then
+    echo "Installing CMake...\n"
+    brew install cmake
+fi
+cmake -S . -B build -G Xcode
+if [ $? -eq 0]; then
+    cmake --build build --config Release
+    if [ $? -eq 1]; then
+        echo "Build failed.\n"
+fi

c8b2e00737bc286880a9fe2873cfa21a5d12a327
YellowCat98 <150300904+YellowCat98@users.noreply.github.com>
2025-01-02 21:18:42 +0100

    oh my god why did dynamic dust make that function name so long

--- src/main.mm
+++ src/main.mm
@@ -4,12 +4,12 @@
 #include <dlfcn.h>
 #include "SteakEngine.hpp"
 
-static void (*KnifeGameLogic_addMeatballs)(id, SEL, long long);
+static bool (*LevelSelectorView_isEnoughMeatballsToUnlockLevel)(id, SEL, long long);
 
-void my_addMeatballs(id self, SEL _cmd, long long param_3) {
+bool my_isEnoughMeatballsToUnlockLevel(id self, SEL _cmd, long long param_3) {
     SteakEngine::log(@"I HOOKED THE FUNCTION!!!!!!!!!!!!!!!!!!");
 
-    return ((void (*)(id, SEL, long long))KnifeGameLogic_addMeatballs)(self, _cmd, param_3);
+    return ((bool (*)(id, SEL, long long))LevelSelectorView_isEnoughMeatballsToUnlockLevel)(self, _cmd, param_3);
 }
 
 __attribute__((constructor))
@@ -19,12 +19,12 @@ static void initialize() {
 
 	SteakEngine::lua::init(L);
 
-	Method method = class_getInstanceMethod(objc_getClass("KnifeGameLogic"), @selector(addMeatballs:param2:param3:));
+	Method method = class_getInstanceMethod(objc_getClass("LevelSelectorView"), @selector(isEnoughMeatballsToUnlockLevel:param2:param3:));
 	IMP original_imp = method_getImplementation(method);
 
-	KnifeGameLogic_addMeatballs = (void (*)(id, SEL, long long))original_imp;
+	LevelSelectorView_isEnoughMeatballsToUnlockLevel = (bool (*)(id, SEL, long long))original_imp;
 
-	rebind_symbols((struct rebinding[1]){{"addMeatballs:param2:param3:", (void *)my_addMeatballs, (void **)&KnifeGameLogic_addMeatballs}}, 1);
+	rebind_symbols((struct rebinding[1]){{"isEnoughMeatballsToUnlockLevel:param2:param3:", (bool *)my_isEnoughMeatballsToUnlockLevel, (bool **)&LevelSelectorView_isEnoughMeatballsToUnlockLevel}}, 1);
 
 	if (luaL_dostring(L, "Log('Hello from lua.')") != LUA_OK) {
 		SteakEngine::log([@"Error: " stringByAppendingString:[NSString stringWithUTF8String:lua_tostring(L, -1)]]);

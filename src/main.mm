50f2a12bf858a66f4f9e56724e6f3344febc04a8
YellowCat98 <150300904+YellowCat98@users.noreply.github.com>
2025-01-02 22:11:45 +0100

    please

--- src/main.mm
+++ src/main.mm
@@ -4,9 +4,9 @@
 #include <dlfcn.h>
 #include "SteakEngine.hpp"
 
-static int (*RunningMinigameViewController_bonusMeatballsGathered)(ID, SEL);
+static int (*RunningMinigameViewController_bonusMeatballsGathered)(id, SEL);
 
-int my_bonusMeatballsGathered(ID self, SEL _cmd) {
+int my_bonusMeatballsGathered(id self, SEL _cmd) {
     SteakEngine::log(@"I HOOKED THE FUNCTION!!!!!!!!!!!!!!!!!!");
 
     return 696969;
@@ -22,9 +22,9 @@ static void initialize() {
 	Method method = class_getInstanceMethod(objc_getClass("RunningMinigameViewController"), @selector(bonusMeatballsGathered:param2:));
 	IMP original_imp = method_getImplementation(method);
 
-	RunningMinigameViewController_bonusMeatballsGathered = (int (*)(ID, SEL))original_imp;
+	RunningMinigameViewController_bonusMeatballsGathered = (int (*)(id, SEL))original_imp;
 
-	rebind_symbols((struct rebinding[1]){{"bonusMeatballsGathered:param2:", (void *)my_bonusMeatballsGathered, (void **)&RunningMinigameViewController_bonusMeatballsGathered}}, 1);
+	rebind_symbols((struct rebinding[1]){{"bonusMeatballsGathered:", (void *)my_bonusMeatballsGathered, (void **)&RunningMinigameViewController_bonusMeatballsGathered}}, 1);
 
 	if (luaL_dostring(L, "Log('Hello from lua.')") != LUA_OK) {
 		SteakEngine::log([@"Error: " stringByAppendingString:[NSString stringWithUTF8String:lua_tostring(L, -1)]]);

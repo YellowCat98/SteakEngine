bb9f51ea37292fd854bb2d0c640380f45240b58f
YellowCat98 <150300904+YellowCat98@users.noreply.github.com>
2025-01-02 22:30:39 +0100

    ok try thattofix it

--- src/main.mm
+++ src/main.mm
@@ -19,12 +19,12 @@ static void initialize() {
 
 	SteakEngine::lua::init(L);
 
-	Method method = class_getInstanceMethod(objc_getClass("RunningMinigameViewController"), @selector(bonusMeatballsGathered:param2));
+	Method method = class_getInstanceMethod(objc_getClass("RunningMinigameViewController"), @selector(bonusMeatballsGathered:));
 	IMP original_imp = method_getImplementation(method);
 
 	RunningMinigameViewController_bonusMeatballsGathered = (int (*)(id, SEL))original_imp;
 
-	rebind_symbols((struct rebinding[1]){{"bonusMeatballsGathered", (void *)my_bonusMeatballsGathered, (void **)&RunningMinigameViewController_bonusMeatballsGathered}}, 1);
+	rebind_symbols((struct rebinding[1]){{"bonusMeatballsGathered:", (void *)my_bonusMeatballsGathered, (void **)&RunningMinigameViewController_bonusMeatballsGathered}}, 1);
 
 	if (luaL_dostring(L, "Log('Hello from lua.')") != LUA_OK) {
 		SteakEngine::log([@"Error: " stringByAppendingString:[NSString stringWithUTF8String:lua_tostring(L, -1)]]);

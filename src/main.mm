2e6acbd1b897a5d11055cd5a7b0e5144e28f04a6
YellowCat98 <150300904+YellowCat98@users.noreply.github.com>
2025-01-02 21:36:37 +0100

    modify that function instead hope that it does Worke..

--- src/main.mm
+++ src/main.mm
@@ -4,12 +4,12 @@
 #include <dlfcn.h>
 #include "SteakEngine.hpp"
 
-static bool (*LevelSelectorView_isEnoughMeatballsToUnlockLevel)(id, SEL, long long);
+static int (*RunningObject_meatballs)(id, SEL);
 
-bool my_isEnoughMeatballsToUnlockLevel(id self, SEL _cmd, long long param_3) {
+int my_meatballs(id self, SEL _cmd) {
     SteakEngine::log(@"I HOOKED THE FUNCTION!!!!!!!!!!!!!!!!!!");
 
-    return ((bool (*)(id, SEL, long long))LevelSelectorView_isEnoughMeatballsToUnlockLevel)(self, _cmd, param_3);
+    return ((int (*)(id, SEL))RunningObject_meatballs)(self, _cmd);
 }
 
 __attribute__((constructor))
@@ -19,12 +19,12 @@ static void initialize() {
 
 	SteakEngine::lua::init(L);
 
-	Method method = class_getInstanceMethod(objc_getClass("LevelSelectorView"), @selector(isEnoughMeatballsToUnlockLevel:param2:param3:));
+	Method method = class_getInstanceMethod(objc_getClass("RunningObject"), @selector(meatballs:param2:));
 	IMP original_imp = method_getImplementation(method);
 
-	LevelSelectorView_isEnoughMeatballsToUnlockLevel = (bool (*)(id, SEL, long long))original_imp;
+	RunningObject_meatballs = (int (*)(id, SEL))original_imp;
 
-	rebind_symbols((struct rebinding[1]){{"isEnoughMeatballsToUnlockLevel:param2:param3:", (bool *)my_isEnoughMeatballsToUnlockLevel, (void **)&LevelSelectorView_isEnoughMeatballsToUnlockLevel}}, 1);
+	rebind_symbols((struct rebinding[1]){{"meatballs:param2:", (void *)my_meatballs, (void **)&RunningObject_meatballs}}, 1);
 
 	if (luaL_dostring(L, "Log('Hello from lua.')") != LUA_OK) {
 		SteakEngine::log([@"Error: " stringByAppendingString:[NSString stringWithUTF8String:lua_tostring(L, -1)]]);

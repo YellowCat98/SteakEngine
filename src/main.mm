fea8e27bd6a9715de44123c57f3d4944fcdc4ebd
YellowCat98 <150300904+YellowCat98@users.noreply.github.com>
2025-01-05 22:50:30 +0100

    oh and

--- src/main.mm
+++ src/main.mm
@@ -9,7 +9,7 @@ static bool (*LevelSelectorView_canSelectLevel)(unsigned long long);
 bool my_canSelectLevel(unsigned long long param_1) {
     SteakEngine::log(@"I HOOKED THE FUNCTION!!!!!!!!!!!!!!!!!!");
 
-    return 696969;
+    return LevelSelectorView_canSelectLevel(param_1);
 }
 
 __attribute__((constructor))

7c25e1f5c5afae4c422c7443ca20efceddb901af
YellowCat98 <150300904+YellowCat98@users.noreply.github.com>
2025-01-13 14:41:26 +0100

    bro.

--- src/main.mm
+++ src/main.mm
@@ -88,7 +88,7 @@ if not label then
 end
 
 label.text = "ASIJODJADIOJDAS"
-label:setFrame(10, 10, 300, 50)
+-- label:setFrame(10, 10, 300, 50)
 view:addSubview(label)
 )") != LUA_OK) {
 		SteakEngine::log([@"\nError: " stringByAppendingString:[NSString stringWithUTF8String:lua_tostring(L, -1)]]);
